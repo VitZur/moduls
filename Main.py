90b32c6cb79ef2ba4e019f8894253f6c4600af31
VitZur <131258965+VitZur@users.noreply.github.com>
2024-08-05 12:59:12 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
 import datetime
-import salary
-import people
+from salary.salary import calculate_salary
+from people.people import get_employees
 
 def main():
     time_now = datetime.datetime.now()
     print(f'Current time now: {time_now}')
 
-    salary_info = salary.calculate_salary()
+    salary_info = calculate_salary()
     print(f'Salary info: {salary_info}')
 
-    employees_info = people.get_employees()
+    employees_info = get_employees()
     print(f'People info: {employees_info}')
 
 if __name__=="__main__":
